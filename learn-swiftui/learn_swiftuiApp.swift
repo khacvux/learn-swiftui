3aca07d0174d669a91bbc0a5ba331ab2b0fe056c
Nguyễn Khắc Vũ <chodiem.io@gmail.com>
2022-08-21 11:27:59 +0700

    Initial Commit

---
+++ learn-swiftui/learn_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftuiApp.swift
+//  learn-swiftui
+//
+//  Created by Vu Khac on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
